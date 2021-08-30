20b645810c2373a6d08f18d04ecd2457ea5b7d9b
bruceyu0925 <81750633+bruceyu0925@users.noreply.github.com>
2021-08-30 13:53:15 +0800

    Add files via upload

--- yenyen/js/block.js
+++ yenyen/js/block.js
@@ -1,30 +1,3 @@
-// 畫面縮放及定位功能
-function AutoWidth() {
-    var Width = document.body.offsetWidth;
-    $('.phone-box').css('width', Width - 400);
-}
-
-function AutoCenter() {
-    var OuterW = document.getElementById('Outer').offsetWidth;
-    var InnerW = document.getElementById('Inner').offsetWidth;
-    var OuterH = document.getElementById('Outer').offsetHeight;
-    var InnerH = document.getElementById('Inner').offsetHeight;
-
-    $('.phone-box').scrollLeft((InnerW - OuterW) / 2);
-    $('.phone-box').scrollTop((InnerH - OuterH) / 2);
-
-    $('.phone').css('transform', 'scale(100%)');
-    $('#Window-Size').val(100);
-    $('.range-text').text('100%');
-}
-
-AutoWidth();
-AutoCenter();
-
-window.onresize = function () {
-    AutoWidth();
-}
-
 // 模式擇一
 $('.choose-one').click(function () {
     $(this).parent().find('.choose-one').removeClass('--click');
